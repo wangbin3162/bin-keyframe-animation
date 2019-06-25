254450d04e2aa2a9a20171c46b5bdb1bcc3b7f3a
wangbin3162 <33084011+wangbin3162@users.noreply.github.com>
2019-06-25 16:45:51 +0800

    修复第一帧问题

--- src/animation.js
+++ src/animation.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var loadImage = require('./imageloader')
 var Timeline = require('./timeline')
+var loadImage = require('./imageloader')
 
 //初始化状态
 var STATE_INITIAL = 0
@@ -9,14 +9,16 @@ var STATE_INITIAL = 0
 var STATE_START = 1
 //停止状态
 var STATE_STOP = 2
+
 //同步任务
 var TASK_SYNC = 0
 //异步任务
 var TASK_ASYNC = 1
 
+
 /**
- * 简单的函数封装来执行回调函数
- * @param callback
+ * 简单的函数封装，执行callback
+ * @param callback 执行的函数
  */
 function next (callback) {
   callback && callback()
@@ -27,45 +29,50 @@ function next (callback) {
  * @constructor
  */
 function Animation () {
-  // 任务队列
   this.taskQueue = []
-  this.index = 0
   this.timeline = new Timeline()
   this.state = STATE_INITIAL
+  this.index = 0
 }
 
 /**
- * 预加载图片，添加同步任务
- * @param imgList 图片数组
+ * 添加一个同步任务，预加载图片
+ * @param imglist 图片数组
  */
-Animation.prototype.loadImage = function (imgList) {
+Animation.prototype.loadImage = function (imglist) {
+
   var taskFn = function (next) {
-    loadImage(imgList.slice(), next)
+    loadImage(imglist.slice(), next)
   }
-  return this._add(taskFn, TASK_SYNC)
+  var type = TASK_SYNC
+
+  return this._add(taskFn, type)
 }
 
 /**
- * 添加异步定时任务，通过定时改变图片背景位置实现帧动画
+ * 添加一个异步定时任务，通过定时改变图片背景位置，实现帧动画
  * @param ele dom对象
  * @param positions 背景位置数组
- * @param imgUrl 图片地址
+ * @param imageUrl 图片地址
  */
-Animation.prototype.changePosition = function (ele, positions, imgUrl) {
+Animation.prototype.changePosition = function (ele, positions, imageUrl) {
   var len = positions.length
-  var taskFn, type
+  var taskFn
+  var type
   if (len) {
     var me = this
     taskFn = function (next, time) {
-      if (imgUrl) {
-        ele.style.backgroundImage = 'url(' + imgUrl + ')'
+      //如果指定图片，则设置dom对象的背景图片地址
+      if (imageUrl) {
+        ele.style.backgroundImage = 'url(' + imageUrl + ')'
       }
-      // 获得当前图片位置索引
-      var index = Math.min(time / me.interval | 0, len - 1)
-      var postion = positions[index].split(' ')
-      // 改变dom对象的背景图片位置
-      ele.style.backgroundPosition = postion[0] + 'px ' + postion[1] + 'px'
-      if (index === len - 1) {
+      //获得当前背景图片位置索引
+      var index = Math.min(time / me.interval | 0, len)
+      var position = positions[index - 1].split(' ')
+      //改变dom对象的背景图片位置
+      ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px'
+      //当前任务执行完毕
+      if (index === len) {
         next()
       }
     }
@@ -79,21 +86,23 @@ Animation.prototype.changePosition = function (ele, positions, imgUrl) {
 }
 
 /**
- * 添加异步定时任务，通过定时改变图片src属性实现帧动画
- * @param ele dom对象
- * @param imgList 图片数组
+ * 添加一个异步定时任务，通过定时改变背景图片地址，实现帧动画
+ * @param ele dom(Image对象)
+ * @param imglist 图片地址数组
  */
-Animation.prototype.changeSrc = function (ele, imgList) {
-  var len = imgList.length
-  var taskFn, type
+Animation.prototype.changeSrc = function (ele, imglist) {
+  var len = imglist.length
+  var taskFn
+  var type
   if (len) {
     var me = this
     taskFn = function (next, time) {
-      // 获得当前图片位置索引
-      var index = Math.min(time / me.interval | 0, len - 1)
-      // 改变img对象的图片地址
-      ele.src = imgList[index]
-      if (index === len - 1) {
+      //获得当前的图片索引
+      var index = Math.min(time / me.interval | 0, len)
+      //改变image对象的图片地址
+      ele.src = imglist[index - 1]
+      //当前任务执行完毕
+      if (index === len) {
         next()
       }
     }
@@ -102,13 +111,14 @@ Animation.prototype.changeSrc = function (ele, imgList) {
     taskFn = next
     type = TASK_SYNC
   }
+
   return this._add(taskFn, type)
 }
 
 /**
- * 高级用法，添加一个异步定时的任务
- * 该任务自定义每帧执行的任务函数
- * @param taskFn
+ * 高级用法，添加一个异步定时执行的任务，
+ * 该任务自定义动画每帧执行的任务函数
+ * @param taskFn 每帧执行的任务函数
  */
 Animation.prototype.enterFrame = function (taskFn) {
   return this._add(taskFn, TASK_ASYNC)
@@ -120,65 +130,72 @@ Animation.prototype.enterFrame = function (taskFn) {
  */
 Animation.prototype.then = function (callback) {
   var taskFn = function (next) {
-    callback()
+    callback(this)
     next()
   }
-  return this._add(taskFn, TASK_SYNC)
+  var type = TASK_SYNC
+
+  return this._add(taskFn, type)
 }
 
 /**
  * 开始执行任务
- * @param interval 异步定时任务的间隔
+ * @param interval 异步定时任务执行的间隔
  */
 Animation.prototype.start = function (interval) {
-  if (this.state === STATE_START) {
+  //如果任务已经开始，则返回
+  if (this.state === STATE_START)
     return this
-  }
-  if (!this.taskQueue.length) {
+  //如果任务链中没有任务，则返回
+  if (!this.taskQueue.length)
     return this
-  }
   this.state = STATE_START
+
   this.interval = interval
   this._runTask()
   return this
 }
 
 /**
- * 添加一个异步同步任务，该任务就是回退到上一个任务中，实现重复上一个任务效果，
+ * 添加一个同步任务，该任务就是回退到上一个任务中，
+ * 实现重复上一个任务的效果，可定义重复的次数。
  * @param times 重复次数
  */
 Animation.prototype.repeat = function (times) {
   var me = this
   var taskFn = function () {
     if (typeof times === 'undefined') {
-      // 不传值就是无限循环
+      //无限回退到上一个任务
       me.index--
       me._runTask()
       return
     }
     if (times) {
       times--
-      //回退
+      //回退到上一个任务
       me.index--
       me._runTask()
     } else {
-      // 达到重复次数跳转到喜爱个任务
+      //达到重复执行次数，则跳转到下一个任务
       var task = me.taskQueue[me.index]
       me._next(task)
     }
   }
-  return this._add(taskFn, TASK_SYNC)
+  var type = TASK_SYNC
+
+  return this._add(taskFn, type)
 }
+
 /**
- * 添加一个异步同步任务，相当于repeat（），无限循环动画
+ * 添加一个同步任务，该任务就是无线循环上一次任务
  */
 Animation.prototype.repeatForever = function () {
   return this.repeat()
 }
 
 /**
- * 设置当前任务执行结束后到下一个任务开始前的等待时间
- * @param time 等待时长
+ * 设置当前任务结束后下一个任务开始前的等待时间
+ * @param time 等待的时长
  */
 Animation.prototype.wait = function (time) {
   if (this.taskQueue && this.taskQueue.length > 0) {
@@ -188,7 +205,7 @@ Animation.prototype.wait = function (time) {
 }
 
 /**
- * 暂停当前的异步定时任务
+ * 暂停当前执行的异步定时任务
  */
 Animation.prototype.pause = function () {
   if (this.state === STATE_START) {
@@ -200,7 +217,7 @@ Animation.prototype.pause = function () {
 }
 
 /**
- * 执行暂停的异步任务
+ * 重新开始执行当前异步定时任务
  */
 Animation.prototype.restart = function () {
   if (this.state === STATE_STOP) {
@@ -245,15 +262,14 @@ Animation.prototype._add = function (taskFn, type) {
  * @private
  */
 Animation.prototype._runTask = function () {
-  if (!this.taskQueue || this.state !== STATE_START) {
+  if (!this.taskQueue || this.state !== STATE_START)
     return
-  }
-  // 如果任务链任务执行完则释放资源
+  //如果任务链任务执行完则释放资源
   if (this.index === this.taskQueue.length) {
     this.dispose()
     return
   }
-  // 获得任务链上的一个任务
+  //获得任务链上的一个任务
   var task = this.taskQueue[this.index]
   if (task.type === TASK_SYNC) {
     this._syncTask(task)
@@ -284,7 +300,7 @@ Animation.prototype._syncTask = function (task) {
  */
 Animation.prototype._asyncTask = function (task) {
   var me = this
-  // 定义每一帧执行的回调函数
+  //定义每一帧执行的回调函数
   var enterframe = function (time) {
     var taskFn = task.taskFn
     var next = function () {
@@ -302,17 +318,23 @@ Animation.prototype._asyncTask = function (task) {
 
 /**
  * 切换到下一个任务，如果当前任务需要等待，则延时执行
- * @param task 当前任务
+ * @param task 下一个任务
  * @private
  */
 Animation.prototype._next = function (task) {
-  this.index++
   var me = this
+  this.index++
   task.wait ? setTimeout(function () {
     me._runTask()
   }, task.wait) : this._runTask()
 }
 
-module.exports = function () {
+
+function createAnimation () {
   return new Animation()
 }
+
+// createAnimation.version = __VERSION__
+
+module.exports = createAnimation
+
